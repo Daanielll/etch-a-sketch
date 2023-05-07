d225ad373d2d076e8cfdb77cf01f12b4f1bd8778
Daniel Moses <dm98912@gmail.com>
2023-05-07 21:41:02 +0300

    Finished all

--- script.js
+++ script.js
@@ -2,22 +2,47 @@
 const container = document.querySelector("#container");
 const slider = document.querySelector(".slider");
 const color = document.querySelector(".color");
-let divs = [];
+const clear = document.querySelector(".clear");
+const eraser = document.querySelector(".eraser");
+const btns = document.querySelectorAll(".btns");
+const label = document.querySelector("label");
 
-// Generates board on slider value change
+let mode = "paint";
+btns.forEach((b) => {
+  b.addEventListener("click", function (e) {
+    if (e.target.localName === "div") return;
+    mode = e.target.classList[0];
+    if (mode === "paint") b.lastElementChild.classList.remove("selected");
+    else b.firstElementChild.classList.remove("selected");
+    e.target.classList.add("selected");
+  });
+});
+// Checks when mouse pressing mouse
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+// Generates board on start and when slider value changes
 generateBoard(slider.value);
-slider.addEventListener("input", function () {
+slider.addEventListener("input", () => {
+  generateBoard(slider.value);
+  label.textContent = `${slider.value}x${slider.value}`;
+});
+clear.addEventListener("click", () => {
+  clearBoard();
   generateBoard(slider.value);
 });
 
 function generateBoard(size) {
   clearBoard();
   for (let i = 0; i < Math.pow(size, 2); i++) {
-    let d = document.createElement("div");
-    d.style.cssText = `outline: 1px solid rgb(224, 234, 242); width: ${
-      1000 / size
-    }px; height: ${750 / size}px;`;
+    const d = document.createElement("div");
+    d.style.cssText = `width: ${700 / size}px; height: ${700 / size}px;`;
     d.addEventListener("mouseover", paint);
+    d.addEventListener("mousedown", () => {
+      if (mode === "paint") d.style.background = color.value;
+      else d.style.background = "white";
+    });
     container.appendChild(d);
   }
 }
@@ -27,16 +52,8 @@ function clearBoard() {
 }
 
 function paint() {
-  this.style.background = color.value;
-}
-/*
-function generateBoard(size) {
-  clearBoard();
-  for (let i = 0; i < size * size; i++) {
-    divs[i] = document.createElement("div");
-    div[i].style.cssText = `outline: 1px solid rgb(224, 234, 242); width: ${
-      1000 / size
-    }px; height: ${750 / size}px;`;
-    container.appendChild(divs[i]);
+  if (mouseDown) {
+    if (mode === "paint") this.style.background = color.value;
+    else if (mode === "eraser") this.style.background = "white";
   }
-} */
+}
